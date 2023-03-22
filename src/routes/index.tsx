6699935e710def676f10949a098766d54754301a
luan huynh <40156001+luanhuynh7181@users.noreply.github.com>
2023-03-22 13:58:16 +0700

    .

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+// import { useRoutes } from 'react-router-dom';
+
+// import { Landing } from '@/features/misc';
+// import { useAuth } from '@/lib/auth';
+
+
+export const AppRoutes = () => {
+  // const auth = useAuth();
+
+  // const commonRoutes = [{ path: '/', element: <Landing /> }];
+
+  // const routes = auth.user ? protectedRoutes : publicRoutes;
+
+  // const element = useRoutes([...routes, ...commonRoutes]);
+
+  return <><div>sdsd</div></>;
+};
